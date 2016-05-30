4bb5189ba4cf0a8f656b34b625da9a7d8cf9a0df
vagrant <vagrant@homestead>
2016-05-30 19:07:16 +0000

    Styling added.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Particle\Validator\Validator;
 require_once '../vendor/autoload.php';
 
 $file = '../storage/database.db';
@@ -12,10 +13,33 @@ $database = new medoo([
 ]);
 
 $comment = new SitePoint\Comment($database);
-$comment->setEmail('bruno@skvorc.me')
-    ->setName('Bruno Skvorc')
-    ->setComment('Hooray! Saving comments works!')
-    ->save();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $v = new Validator();
+    $v->required('name')->lengthBetween(1, 100)->alnum(true);
+    $v->required('email')->email()->lengthBetween(5, 255);
+    $v->required('comment')->lengthBetween(10, null);
+
+    $result = $v->validate($_POST);
+
+    if ($result->isValid()) {
+        try {
+            $comment
+                ->setName($_POST['name'])
+                ->setEmail($_POST['email'])
+                ->setComment($_POST['comment'])
+                ->save();
+
+            header('Location: /');
+            return;
+
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
+    } else {
+        dump($result->getMessages());
+    }
+}
 ?>
 <!doctype html>
 <html class="no-js" lang="">
@@ -31,6 +55,7 @@ $comment->setEmail('bruno@skvorc.me')
 
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/custom.css">
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
     <body>
@@ -39,6 +64,13 @@ $comment->setEmail('bruno@skvorc.me')
         <![endif]-->
 
         <!-- Add your site or application content here -->
+        <?php foreach ($comment->findAll() as $comment) : ?>
+            <div class="comment">
+                <h3>On <?php $comment->getSubmissionDate() ?>, <?= $comment->getName() ?> wrote:</h3>
+                <p><?php $comment->getComment(); ?></p>
+            </div>
+        <?php endforeach; ?>
+
         <form method="post">
                 <label>Name: <input type="text" name="name" placeholder="Your name"></label>
                 <label>Email: <input type="text" name="email" placeholder="your@email.com"></label>
